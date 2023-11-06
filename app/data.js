2c3bc7180fc6a609d1c2544e6af2ca8d5601b440
Mohammad Enayet <enayetmd590@gmail.com>
2023-11-06 11:50:41 +0600

    MERN Project 02 FaceBook Timeline

---
+++ app/data.js
@@ -0,0 +1,75 @@
+const post = [
+  {
+    id: 1,
+    image:
+      "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/399434938_733762815235998_3261307115003528072_n.jpg?stp=cp6_dst-jpg&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG8baNlYvT7Bl1PRBFo6Nm8YpHKCAz7aAZikcoIDPtoBrneqXpIeiKeEeIscrM_JSXEKOPrDv-VQ1ADa4WhwuhZ&_nc_ohc=nq-0t_haxQoAX-QVC5j&_nc_ht=scontent.fdac7-1.fna&oh=00_AfASApFbl6CgubLg-RtETmrOnTce_XFKunSyfD1avwA-Lw&oe=654C74C1",
+    content:
+      "I have become one of the rare people who don't know how to quit...",
+    like: 300,
+    Comment: 20,
+    timeAgo: "20 min",
+    author: {
+      name: "Jubayer Hossain",
+      photo:
+        "https://scontent.fdac7-1.fna.fbcdn.net/v/t1.6435-9/140365170_129602182318734_2789957791821544474_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=be3454&_nc_eui2=AeGEzLesRXM-6GkqPdDIMjNCs_oRMKc7jyWz-hEwpzuPJakfTPQOmLodbggi_Yo30VIs9v56LMq20Ly-gze4zxVr&_nc_ohc=yUYXx8nikuAAX-IQKmc&_nc_ht=scontent.fdac7-1.fna&oh=00_AfDd6v0PnmYF5q52Shm_fjuuaqJgNI5SessjNYgq83-k7g&oe=656FD958",
+    },
+  },
+  {
+    id: 2,
+    image:
+      "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/398949635_122116071410076266_689047229114352272_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGH2WDY69806zEUU3KdPaEztBGtO2Z6_6y0Ea07Znr_rE6-Y6FwQ6OuKXmZPuwXiPtBiiNUWlyDQ1eYW3HJM0Iz&_nc_ohc=ACedHBLPPTEAX-hZmBT&_nc_ht=scontent.fdac7-1.fna&oh=00_AfDkALPIbEZHWpJnt5g-3wUhhp-OCiBMdTwFJ8c3cdT-ig&oe=654C86F9",
+    content: "পরিবার ছাড়া জীবনের কোন মানে নেই। ❤️",
+    like: 279,
+    Comment: 300,
+    timeAgo: "a day ago ",
+    author: {
+      name: "Tawhid Afridi official Fans",
+      photo:
+        "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/395674868_122109814652076266_6188052555269572519_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF4AmacixCjC30E1ZWYAKMqKoqljx1N7r0qiqWPHU3uvRUB6naJ5yqOytfbyErRLpnXZP1HyMnbcw2KbhEi_kHJ&_nc_ohc=FHoOgndGquwAX-CefhW&_nc_ht=scontent.fdac7-1.fna&oh=00_AfD5h9D29fgxJ2nJ481gEU6EKhzZ_kp7x5Idbu2OIuflIg&oe=654E14F0",
+    },
+  },
+  {
+    id: 3,
+    image:
+      "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/398724965_687460296676465_7151597863644483240_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHPuIngm9a0t-TBNp3oXKeuVuPjzayUIaFW4-PNrJQhodYv-NY1lJCCa2ystM7Pml61Wz5qgr-2HoYqdazUslkW&_nc_ohc=CuoY40XfvE0AX9la3m-&_nc_oc=AQnmdggpeaVkoUZfZUE9tsY37z4EycLrOlrPqd_9neEn5yx3Kl6FzNNGEXJH_45c3z8&_nc_ht=scontent.fdac7-1.fna&oh=00_AfB-RTYJ7JyoMNYpidkDcch8VInTGukZD_OgIMOQNuUohg&oe=654DC992",
+    content: "The King 👑🔥",
+    like: 215,
+    Comment: "2k",
+    timeAgo: "5 min",
+    author: {
+      name: "Ahana Jain ",
+      photo:
+        "https://scontent.fdac7-1.fna.fbcdn.net/v/t1.6435-9/141304968_105715424850958_7128847189234567174_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=be3454&_nc_eui2=AeH-xAqCjsMdRoNpOfP8560ehr4RdWHAgvOGvhF1YcCC88TkR19LQZZIDOQSBW0gpzy4lffIcUyf3k3UJfap_HxT&_nc_ohc=8fVV0CRQhHAAX8FsKI_&_nc_ht=scontent.fdac7-1.fna&oh=00_AfD4SICANPvldcUmhX4mS069zLvLTThVCsziZFgB_nM1oQ&oe=656FE430",
+    },
+  },
+  {
+    id: 4,
+    image:
+      "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/399402941_741837581304324_8086331302835791471_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHyjVO7ziMkiMuhuTC_FFnEQG04aB-ImkZAbThoH4iaRlH-rbjIz-z0mO96A39qdRYNK7OoEEC3PBpbTWIwc0CI&_nc_ohc=4qc4hSEDSFcAX-xhKj0&_nc_ht=scontent.fdac7-1.fna&oh=00_AfC__K9uXJBd7rUMA00ODZlHb1I7rXE9VsuYJtzu2W3M_w&oe=654D2C13",
+    content: `Congratulations বাংলাদেশ বডি বিল্ডিং এর গর্ব Mr olympia champion #নসু_দা  😍
+    ￼￼ our national icon ❤️`,
+    like: 500,
+    Comment: "25",
+    timeAgo: "5 hour",
+    author: {
+      name: "Jahid Hasan Shuvo",
+      photo:
+        "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/346060618_511135744424645_3736838867328895626_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEapdcwMvOO1XxNEVlsXF-wzClc2mORIczMKVzaY5EhzLI4EXPtiqFB2hnjY1BplWL2uCErId-x4Wrnw-gT60-w&_nc_ohc=kpatucojIAkAX9IhR0o&_nc_ht=scontent.fdac7-1.fna&oh=00_AfAcaXRTAnrQ7HsASbnWEol4foDMhRZAYLy8qjYZOruCTQ&oe=654DD923",
+    },
+  },
+  {
+    id: 5,
+    image:
+      "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/399410565_3222151308088487_1990907489077624655_n.jpg?stp=dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGv9eal3e7VcjRfzFRa3r114XEOYniQLirhcQ5ieJAuKo9HRGEsYiKiGWNE_EQ3pDgqHWTXchAxHzuS6kYGHHdm&_nc_ohc=_dsbf-BgUlcAX-fPnSJ&_nc_ht=scontent.fdac7-1.fna&oh=00_AfDIk4ZqXe6-tblb_tQZfnzC9rfcFy8lr4lfu9rrWnTnVg&oe=654D3350",
+    content: `আলহামদুলিল্লাহ,
+    গতকাল রাতে ৪০০$ একটা মিটিং করেছি ৭৫$ অগ্রীম দিয়েছে বাকী পেমেন্ট সামনে সপ্তাহে দিবে।`,
+    like: 245,
+    Comment: "30",
+    timeAgo: "15 min",
+    author: {
+      name: "Ranzu Hasan Raj",
+      photo:
+        "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/362910197_3161786504124968_8364483668425749139_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeELeEsJcyUuZZETLHvKiKOqG4piUdMTTOsbimJR0xNM60-BukeQCJdIl-Ef4HtXb2Jl3lNTQif1qhuwwOrPd8wD&_nc_ohc=N_iwhRvNtzMAX_7RjIq&_nc_ht=scontent.fdac7-1.fna&oh=00_AfCJ0Ac8YiA2oaDwPbrzvMs0aaz-EEjhKpWCVJCVh14Ngg&oe=654E1B60",
+    },
+  },
+];
